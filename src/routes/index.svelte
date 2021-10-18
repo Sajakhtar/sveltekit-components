7d4c464236ef264061ace9518944121ecb9d8d2d
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-10-18 12:47:20 +0400

    create accordion component with mounting and unmounting from the dom

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,18 @@
 <script>
   import Toggle from '$lib/Toggle.svelte';
+  import Accordion from '$lib/Accordion.svelte';
 
   let isToggled = false;
 </script>
 
 <h1>Welcome to Levelup UI SvelteKit Components</h1>
+
+
+<Accordion isOpen={true} buttonText={"FAQ: How to reset password?"} >
+  <div>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem reprehenderit fugiat sed voluptatem nobis aspernatur dolore accusamus atque dolores corporis ullam, totam optio facilis esse maiores inventore, laudantium consequuntur laboriosam!</p>
+  </div>
+</Accordion>
 
 <Toggle bind:isToggled label="Beta" />
 <Toggle bind:isToggled label="Beta" style="--toggleBackgroundColor: #b7e4c7" />
