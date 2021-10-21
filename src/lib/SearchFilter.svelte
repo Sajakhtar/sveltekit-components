92c97b306f45522b73818b4838c12f884826a5a0
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-10-21 16:02:15 +0400

    create filter search component with case sensitive input to filter a list

---
+++ src/lib/SearchFilter.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let items;
+  let search =  '';
+
+  $: filteredSearch = items.filter((item) => {
+    return search === '' || item.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+  });
+</script>
+
+<label>
+  Search names:
+  <br>
+  <input type="text" bind:value={search}>
+</label>
+
+<ul>
+  {#each filteredSearch as item}
+  <li>{item}</li>
+  {/each}
+</ul>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,28 +2,34 @@
   import Toggle from '$lib/Toggle.svelte';
   import Accordion from '$lib/Accordion.svelte';
   import BetterAccordion from '$lib/BetterAccordion.svelte';
+  import SearchFilter from '$lib/SearchFilter.svelte';
 
   let isToggled = false;
+
+  let items = ['Jack', 'Jon', 'Jill', 'Jane', 'Joe ', 'Jim', 'John', 'Jen'];
 </script>
 
 <h1>Welcome to Levelup UI SvelteKit Components</h1>
 
 
-<Accordion isOpen={true} buttonText={"FAQ: How to reset password?"} >
+<!-- <Accordion isOpen={true} buttonText={"FAQ: How to reset password?"} >
   <div>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem reprehenderit fugiat sed voluptatem nobis aspernatur dolore accusamus atque dolores corporis ullam, totam optio facilis esse maiores inventore, laudantium consequuntur laboriosam!</p>
   </div>
-</Accordion>
+</Accordion> -->
 
-<Toggle bind:isToggled label="Beta" />
-<Toggle bind:isToggled label="Beta" style="--toggleBackgroundColor: #b7e4c7" />
+<!-- <Toggle bind:isToggled label="Beta" />
+<Toggle bind:isToggled label="Beta" style="--toggleBackgroundColor: #b7e4c7" /> -->
 
 {#if isToggled}
   <h1>I'm toggled</h1>
 {/if}
 
-<BetterAccordion isOpen={false} buttonText={"Better Accordion: FAQ- How to reset password?"} >
+<!-- <BetterAccordion isOpen={false} buttonText={"Better Accordion: FAQ- How to reset password?"} >
   <div>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem reprehenderit fugiat sed voluptatem nobis aspernatur dolore accusamus atque dolores corporis ullam, totam optio facilis esse maiores inventore, laudantium consequuntur laboriosam!</p>
   </div>
-</BetterAccordion>
+</BetterAccordion> -->
+
+
+<SearchFilter {items} />
